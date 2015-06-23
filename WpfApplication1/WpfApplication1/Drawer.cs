add28bf90f15319869472c3f4ef2ffaf13b2c565
Guoliang <guolaing.zhao14@imperial.ac.uk>
2015-06-23 16:52:43 +0100

    add resize window functionality and refactor

---
+++ WpfApplication1/WpfApplication1/Drawer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace WpfApplication1
+{
+    public class Drawer
+    {
+        private readonly List<Tuple<JointType, JointType>> bones; 
+        public Drawer()
+        {
+            // a bone defined as a line between two joints
+            this.bones = new List<Tuple<JointType, JointType>>();
+
+            // Torso
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.Head, JointType.Neck));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.Neck, JointType.SpineShoulder));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.SpineMid));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineMid, JointType.SpineBase));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.ShoulderRight));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.ShoulderLeft));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineBase, JointType.HipRight));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineBase, JointType.HipLeft));
+
+            // Right Arm
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.ShoulderRight, JointType.ElbowRight));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.ElbowRight, JointType.WristRight));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristRight, JointType.HandRight));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.HandRight, JointType.HandTipRight));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristRight, JointType.ThumbRight));
+
+            // Left Arm
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.ShoulderLeft, JointType.ElbowLeft));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.ElbowLeft, JointType.WristLeft));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristLeft, JointType.HandLeft));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.HandLeft, JointType.HandTipLeft));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristLeft, JointType.ThumbLeft));
+
+            // Right Leg
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.HipRight, JointType.KneeRight));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.KneeRight, JointType.AnkleRight));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.AnkleRight, JointType.FootRight));
+
+            // Left Leg
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.HipLeft, JointType.KneeLeft));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.KneeLeft, JointType.AnkleLeft));
+            this.bones.Add(new Tuple<JointType, JointType>(JointType.AnkleLeft, JointType.FootLeft));
+        }
+        
+
+        public void showHands(DepthSpacePoint rightHand, DepthSpacePoint leftHand, HandState rightHandState, HandState leftHandState, Canvas bodyCanvas)
+        {
+            Brush openBrush = new SolidColorBrush(Color.FromArgb(100, 120, 5, 250));
+            Brush closeBrush = new SolidColorBrush(Color.FromArgb(100, 0, 200, 250));
+
+            Brush rightBrush = (rightHandState == HandState.Closed ? closeBrush : openBrush);
+            drawCircle(50, rightHand.X, rightHand.Y, rightBrush, bodyCanvas);
+
+            Brush leftBrush = (leftHandState == HandState.Closed ? closeBrush : openBrush);
+            drawCircle(50, leftHand.X, leftHand.Y, leftBrush, bodyCanvas);
+            
+        }
+
+        public void drawBones(Dictionary<JointType, DepthSpacePoint> jointPoints, Canvas bodyCanvas)
+        {
+            foreach (Tuple<JointType, JointType> bone in bones)
+            {
+                JointType end1 = bone.Item1;
+                JointType end2 = bone.Item2;
+
+                if (!jointPoints.ContainsKey(end1) || !jointPoints.ContainsKey(end2))
+                {
+                    continue;
+                }
+
+                Line myLine = new Line
+                {
+                    X1 = jointPoints[end1].X,
+                    X2 = jointPoints[end2].X,
+                    Y1 = jointPoints[end1].Y,
+                    Y2 = jointPoints[end2].Y,
+                    Stroke = new SolidColorBrush(Color.FromArgb(255, 0, 255, 255)),
+                    StrokeThickness = 2
+
+                };
+
+                bodyCanvas.Children.Add(myLine);
+
+            }
+        }
+
+        public void drawCircle(int radius, float X, float Y, Brush color, Canvas bodyCanvas)
+        {
+            Ellipse leftHandEllipse = new Ellipse()
+            {
+                Height = radius,
+                Width = radius,
+                Fill = color
+            };
+            bodyCanvas.Children.Add(leftHandEllipse);
+            Canvas.SetLeft(leftHandEllipse, X - radius / 2);
+            Canvas.SetTop(leftHandEllipse, Y - radius / 2);
+        }
+    }
+}
--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -33,58 +33,22 @@ namespace WpfApplication1
 
         private Body[] bodies;
         private BodyFrameReader bfr;
-        private List<Tuple<JointType, JointType>> bones;
 
-        [DllImport("User32.dll")]
-        private static extern bool SetCursorPos(int X, int Y);
+        private Drawer drawer;
+        private Manipulation man;
+        
 
-        [DllImport("User32.dll")]
-        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
-
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private int offset = 0;
 
         public MainWindow()
         {
             InitializeComponent();
+            drawer = new Drawer();
+            man = new Manipulation();
+
             //moveCursor();
 
-            // a bone defined as a line between two joints
-            this.bones = new List<Tuple<JointType, JointType>>();
-
-            // Torso
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.Head, JointType.Neck));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.Neck, JointType.SpineShoulder));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.SpineMid));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineMid, JointType.SpineBase));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.ShoulderRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.ShoulderLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineBase, JointType.HipRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineBase, JointType.HipLeft));
-
-            // Right Arm
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.ShoulderRight, JointType.ElbowRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.ElbowRight, JointType.WristRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristRight, JointType.HandRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.HandRight, JointType.HandTipRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristRight, JointType.ThumbRight));
-
-            // Left Arm
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.ShoulderLeft, JointType.ElbowLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.ElbowLeft, JointType.WristLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristLeft, JointType.HandLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.HandLeft, JointType.HandTipLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristLeft, JointType.ThumbLeft));
-
-            // Right Leg
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.HipRight, JointType.KneeRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.KneeRight, JointType.AnkleRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.AnkleRight, JointType.FootRight));
-
-            // Left Leg
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.HipLeft, JointType.KneeLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.KneeLeft, JointType.AnkleLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.AnkleLeft, JointType.FootLeft));
+            
 
             this.Loaded += MainPage_Loaded;
         }
@@ -98,6 +62,7 @@ namespace WpfApplication1
 
             sensor.Open();
             bfr.FrameArrived += bfr_FrameArrived;
+            
 
         }
 
@@ -105,143 +70,87 @@ namespace WpfApplication1
         {
             using (BodyFrame bodyFrame = args.FrameReference.AcquireFrame())
             {
-                if (bodyFrame != null)
+                if (bodyFrame == null)
                 {
-                    bodyFrame.GetAndRefreshBodyData(bodies);
-                    bodyCanvas.Children.Clear();
+                    return;
+                }
+                bodyFrame.GetAndRefreshBodyData(bodies);
+                bodyCanvas.Children.Clear();
 
-                    foreach (var body in bodies)
+                foreach (var body in bodies)
+                {
+                    if (!body.IsTracked)
                     {
-                        if (!body.IsTracked)
+                        continue;
+                    }
+
+                    var jointsDic = body.Joints;
+                    Dictionary<JointType, DepthSpacePoint> joints = new Dictionary<JointType, DepthSpacePoint>();
+                    foreach (var joint in jointsDic)
+                    {
+                        if (joint.Value.TrackingState == TrackingState.NotTracked)
                         {
+                            Debug.Print("Hey");
                             continue;
                         }
 
-                        var jointsDic = body.Joints;
-                        Dictionary<JointType, DepthSpacePoint> joints = new Dictionary<JointType, DepthSpacePoint>();
-                        foreach (var joint in jointsDic)
-                        {
-                            if (joint.Value.TrackingState == TrackingState.NotTracked)
-                            {
-                                Debug.Print("Hey");
-                                continue;
-                            }
+                        DepthSpacePoint point =
+                            sensor.CoordinateMapper.MapCameraPointToDepthSpace(joint.Value.Position);
 
-                            DepthSpacePoint point =
-                                sensor.CoordinateMapper.MapCameraPointToDepthSpace(joint.Value.Position);
+                        joints.Add(joint.Key, point);
+                    }
+                        
+                    drawer.drawBones(joints, bodyCanvas);
+                    drawer.showHands(joints[JointType.HandRight], joints[JointType.HandLeft],
+                        body.HandRightState, body.HandLeftState, bodyCanvas);
 
-                            joints.Add(joint.Key, point);
-                        }
+                    foreach (var joint in joints.Values)
+                    {
+                        drawer.drawCircle(10, joint.X, joint.Y, new SolidColorBrush(Color.FromArgb(255, 100, 255, 100)), bodyCanvas);
+                    }
 
-                        DrawBones(joints);
-                        showHands(joints[JointType.HandRight], joints[JointType.HandLeft],
-                            body.HandRightState, body.HandLeftState);
-                        foreach (var joint in joints.Values)
-                        {
-                            Ellipse circle = new Ellipse
-                            {
-                                Width = 10,
-                                Height = 10,
+                    if (!joints.ContainsKey(JointType.HandRight))
+                    {
+                        continue;
+                    }
 
-                                Fill = new SolidColorBrush(Color.FromArgb(255, 100, 255, 100))
-                            };
+                    int a = (int) (joints[JointType.HandRight].X / 514.0 * SystemParameters.PrimaryScreenWidth);
+                    int b = (int)(joints[JointType.HandRight].Y / 424.0 * SystemParameters.PrimaryScreenHeight * 2);
 
-                            bodyCanvas.Children.Add(circle);
-                            Canvas.SetLeft(circle, joint.X - 5);
-                            Canvas.SetTop(circle, joint.Y - 5);
-                        }
-
-                        //Debug.Print(joints.ContainsKey(JointType.HandRight).ToString());
-                        if (joints.ContainsKey(JointType.HandRight))
-                        {
-                            int a = (int) (joints[JointType.HandRight].X / 514.0 * SystemParameters.PrimaryScreenWidth);
-                            int b = (int)(joints[JointType.HandRight].Y / 424.0 * SystemParameters.PrimaryScreenHeight * 2);
-
-                            Debug.Print(a.ToString());
-                            Debug.Print(b.ToString());
+//                            Debug.Print(a.ToString());
+//                            Debug.Print(b.ToString());
 
 //                            Debug.Print(SystemParameters.PrimaryScreenWidth.ToString("N"));
 //                            Debug.Print(SystemParameters.PrimaryScreenHeight.ToString("N"));
 
-                            moveCursor(a, b);
+                    man.moveCursor(a, b);
                             
-                            if (body.HandRightState == HandState.Closed)
-                            {
-                                Debug.Print("Closed");
-                                leftClick(a, b); 
-                            }
+                    if (body.HandRightState == HandState.Closed)
+                    {
+                        Debug.Print("Closed");
+                        man.leftClick(a, b);
+
+                        if (body.HandLeftState == HandState.Closed)
+                        {
+                            int la = (int)(joints[JointType.HandLeft].X / 514.0 * SystemParameters.PrimaryScreenWidth);
+                            int lb = (int)(joints[JointType.HandLeft].Y / 424.0 * SystemParameters.PrimaryScreenHeight * 2);
+                                    
+                            int dis = (int) Math.Sqrt(Math.Pow((la - a), 2) + Math.Pow((lb - b), 2));
+
+                            man.moveWindow(dis);
+//                                    if (offset < 500)
+//                                    {
+//                                        offset +=  50;
+//                                    }
+                            Debug.Print(offset.ToString());
+
                         }
 
                     }
-
                 }
             }
         }
-
-        private void showHands(DepthSpacePoint rightHand, DepthSpacePoint leftHand,
-            HandState rightHandState, HandState leftHandState)
-        {
-            Brush openBrush = new SolidColorBrush(Color.FromArgb(100, 120, 5, 250));
-            Brush closeBrush = new SolidColorBrush(Color.FromArgb(100, 0, 200, 250));
-
-            Ellipse rightHandEllipse = new Ellipse()
-            {
-                Height = 50,
-                Width = 50,
-                Fill = (rightHandState == HandState.Closed ? closeBrush : openBrush)
-            };
-            bodyCanvas.Children.Add(rightHandEllipse);
-            Canvas.SetLeft(rightHandEllipse, rightHand.X - 25);
-            Canvas.SetTop(rightHandEllipse, rightHand.Y - 25);
-
-            Ellipse leftHandEllipse = new Ellipse()
-            {
-                Height = 50,
-                Width = 50,
-                Fill = (leftHandState == HandState.Closed ? closeBrush : openBrush)
-            };
-            bodyCanvas.Children.Add(leftHandEllipse);
-            Canvas.SetLeft(leftHandEllipse, leftHand.X - 25);
-            Canvas.SetTop(leftHandEllipse, leftHand.Y - 25);
-        }
-
-        private void DrawBones(Dictionary<JointType, DepthSpacePoint> jointPoints)
-        {
-            foreach (Tuple<JointType, JointType> bone in bones)
-            {
-                JointType end1 = bone.Item1;
-                JointType end2 = bone.Item2;
-
-                if (!jointPoints.ContainsKey(end1) || !jointPoints.ContainsKey(end2))
-                {
-                    continue;
-                }
-
-                Line myLine = new Line
-                {
-                    X1 = jointPoints[end1].X,
-                    X2 = jointPoints[end2].X,
-                    Y1 = jointPoints[end1].Y,
-                    Y2 = jointPoints[end2].Y,
-                    Stroke = new SolidColorBrush(Color.FromArgb(255, 0, 255, 255)),
-                    StrokeThickness = 2
-
-                };
-
-                bodyCanvas.Children.Add(myLine);
-
-            }
-        }
-
-        private void moveCursor(int a, int b)
-        {
-            SetCursorPos(a, b);
-        }
-
-        private void leftClick(int a, int b)
-        {
-            mouse_event(MOUSEEVENTF_LEFTDOWN, a, b, 0, 0);
-            mouse_event(MOUSEEVENTF_LEFTUP, a, b, 0, 0);
-        }
+         
+        
     }
 }
